205d5f2c0f6bfcecf03c5bf34228cdfbb5b4ea3a
taaaaaaak22 <takahi1210@gmail.com>
2020-09-16 06:54:32 +0900

    修正

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const video = document.getElementById('video')
-video.addEventListener('loadedmetadata', () => {
+video.addEventListener('loadeddata', () => {
   const totalSec = video.duration
   document.getElementById('wrapper').style.height = Math.floor(totalSec) * 500 + 'px'
+  window.requestAnimationFrame(scrollVideo)
 })
 
 function scrollVideo() {
   video.currentTime = window.pageYOffset / 500
   window.requestAnimationFrame(scrollVideo)
 }
-window.requestAnimationFrame(scrollVideo)
