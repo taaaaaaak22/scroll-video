df5530c8eb9cb623acabb17b5e66af322a4d1213
taaaaaaak22 <takahi1210@gmail.com>
2020-09-16 06:44:56 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const video = document.getElementById('video')
+const totalSec = video.duration
+console.log(totalSec);
+document.getElementById('wrapper').style.height = Math.floor(totalSec) * 500 + 'px'
+
+function scrollVideo() {
+  video.currentTime = window.pageYOffset / 500
+  window.requestAnimationFrame(scrollVideo)
+}
+window.requestAnimationFrame(scrollVideo)
